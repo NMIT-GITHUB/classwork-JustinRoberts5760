2964ef9b3832e38d1367a1dcfe5ba9672f3383b4
justin roberts <Justin-Roberts2@live.nmit.ac.nz>
2025-02-24 14:51:37 +1300

    added crud functionality

--- _student app/app.js
+++ _student app/app.js
@@ -34,6 +34,32 @@ app.post("/new-entry", (req,res) => {
     res.redirect("/")
 })
 
+app.get("/edit-entry/:id", (req,res) => {
+    const id = parseInt(req.params.id)
+    const entry = entries.find(e => e.id === id)
+    if(!entry) {
+        res.status(404).send("Entry not found")
+        return
+    }
+    res.render("edit-entry", {entry})
+})
+
+app.post("/edit-entry/:id", (req,res) => {
+    const id = parseInt(req.params.id)
+    const entry = entries.find(e => e.id === id)
+    if (!entry) {
+        res.status(404).send("Entry not found")
+        return
+    }
+    if (!req.body.title || !req.body.body) {
+        res.status(400).send("Both title and text are required!")
+    }
+    entry.title = req.body.title
+    entry.body = req.body.body
+    entry.published = new Date()
+    res.redirect("/")
+})
+
 app.use((res,req) => {
     res.status(404).render("404")
 })
